32d0cb82721fe17f43c05e88b239f10563d756b0
SylwekWK <141858138+SylwekWK@users.noreply.github.com>
2026-09-09 18:50:38 +0200

    Improve game loop and event handling methods
    Refactor game loop and event handling for better structure.

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,22 +14,40 @@ class AlienInvasion:
         (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Inwazja obcych")
         self.ship = Ship(self)
-        # Zdefiniowanie koloru tła.
-        self.bg_color = (23, 35, 230)
     def run_game(self):
         """Rozpoczęcie pętli głównej gry."""
         while True: 
         # Oczekiwanie na naciśnięcie klawisza lub przycisku myszy.
-            for event in pygame.event.get(): 
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _check_events(self):
+         for event in pygame.event.get(): 
                 if event.type == pygame.QUIT: 
                     sys.exit()
-                # Odświeżenie ekranu w trakcie każdej iteracji pętli.
+                elif event.type==pygame.KEYDOWN:
+                    if event.key==pygame.K_RIGHT:
+                        self.ship.moving_right=True
+                    elif event.key==pygame.K_LEFT:
+                        self.ship.moving_left=True
+
+                elif event.type==pygame.KEYUP:
+                    if event.key==pygame.K_RIGHT:
+                        self.ship.moving_right=False
+                    elif event.type==pygame.KEYUP:
+                        self.ship.moving_left=False
+
+    def _update_screen(self):
+            # Odświeżenie ekranu w trakcie każdej iteracji pętli.
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
                     # Wyświetlenie ostatnio zmodyfikowanego ekranu.
             pygame.display.flip()
-            self.clock.tick(60)
+        
 if __name__ == '__main__':
     # Utworzenie egzemplarza gry i jej uruchomienie.
     ai = AlienInvasion()
     ai.run_game()                                    
+()                                    
