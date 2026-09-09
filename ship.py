16c0161c9766b900bbab75f3c89a71cca879a9cf
SylwekWK <141858138+SylwekWK@users.noreply.github.com>
2026-09-09 18:51:18 +0200

    Enhance Ship class with movement and settings
    Added movement functionality and settings integration for the ship.

--- ship.py
+++ ship.py
@@ -5,12 +5,23 @@ class Ship:
         """Inicjalizacja statku kosmicznego i jego położenie oczątkowe."""
     
         self.screen = ai_game.screen 
+        self.settings=ai_game.settings
         self.screen_rect = ai_game.screen.get_rect() 
         # Wczytanie obrazu statku kosmicznego i pobranie jego prostokąta.
-        self.image = pygame.image.load('Image/ship.bmp') 
+        self.image = pygame.image.load('Images/ship.bmp') 
         self.rect = self.image.get_rect()
         # Każdy nowy statek kosmiczny pojawia się na dole ekranu.
         self.rect.midbottom = self.screen_rect.midbottom 
+        self.x=float(self.rect.x)
+        self.moving_right=False
+        self.moving_left=False
+    def update (self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x+=self.settings.ship_speed
+        if self.moving_left and self.rect.left>0:
+            self.x-=self.settings.ship_speed
+        self.rect.x=self.x
+
     def blitme(self): 
         """Wyświetlenie statku kosmicznego w jego aktualnym położeniu."""
         self.screen.blit(self.image, self.rect)
