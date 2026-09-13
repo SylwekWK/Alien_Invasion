ba7cb7ede28e7a623abc62ff4e30df20e583d1f1
SylwekWK <141858138+SylwekWK@users.noreply.github.com>
2026-09-13 22:04:26 +0200

    Enhance key event handling and adjust game loop speed
    Refactor event handling and game loop for improved control.

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,23 +21,38 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_screen()
-            self.clock.tick(60)
+            self.clock.tick(10)
 
     def _check_events(self):
          for event in pygame.event.get(): 
                 if event.type == pygame.QUIT: 
                     sys.exit()
                 elif event.type==pygame.KEYDOWN:
-                    if event.key==pygame.K_RIGHT:
-                        self.ship.moving_right=True
-                    elif event.key==pygame.K_LEFT:
-                        self.ship.moving_left=True
-
+                    self._check_keydown_events(event)
                 elif event.type==pygame.KEYUP:
-                    if event.key==pygame.K_RIGHT:
-                        self.ship.moving_right=False
-                    elif event.type==pygame.KEYUP:
-                        self.ship.moving_left=False
+                    self._check_keyup_events(event)
+                
+    def _check_keydown_events(self, event):
+        if event.key==pygame.K_RIGHT:
+            self.ship.moving_right=True
+        if event.key==pygame.K_LEFT:
+            self.ship.moving_left=True
+        if event.key==pygame.K_UP:
+            self.ship.moving_up=True
+        if event.key==pygame.K_DOWN:
+            self.ship.moving_down=True
+        if event.key==pygame.K_q:
+            sys.exit()
+        
+    def _check_keyup_events(self, event):
+        if event.key==pygame.K_RIGHT:
+            self.ship.moving_right=False
+        if event.key==pygame.K_LEFT:
+            self.ship.moving_left=False
+        if event.key==pygame.K_UP:
+            self.ship.moving_up=False
+        if event.key==pygame.K_DOWN:
+            self.ship.moving_down=False
 
     def _update_screen(self):
             # Odświeżenie ekranu w trakcie każdej iteracji pętli.
@@ -46,8 +61,7 @@ class AlienInvasion:
                     # Wyświetlenie ostatnio zmodyfikowanego ekranu.
             pygame.display.flip()
 
-
 if __name__ == '__main__':
     # Utworzenie egzemplarza gry i jej uruchomienie.
     ai = AlienInvasion()
-    ai.run_game()                                    
+    ai.run_game()                                                                      
